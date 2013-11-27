39955b83efac0ffcbdba744e88a09c856d0a4117
boyishwei <boyishwei@gmail.com>
2013-11-27 03:10:44 -0800

    upload sponor

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -16,6 +16,8 @@ class ImagesController < ApplicationController
   def show
     @image = Image.find(params[:id])
 
+    @images = Image.page(1).per(10)
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @image }
